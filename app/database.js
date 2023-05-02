2490f9365520c335f284515a08ab890693e5756e
Cristian Bazán <98223399+CristianBazan-dev@users.noreply.github.com>
2023-05-02 00:35:51 -0300

    Update database.js

--- app/database.js
+++ app/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { ATLAS_URI  } = require("./config"); 
 
 mongoose
-    .connect("mongodb+srv://impresionesbraminfd:g689sJWe8c2hCBys@cluster0.0j31isd.mongodb.net/test")
+    .connect()
 
     .then((db) => console.log("DB is connected"))
     .catch((err) => console.log(err))
